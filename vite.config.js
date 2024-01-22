efd02445b99eae414414b43d5714eef2175e7613
Yu-cheng Zheng <yu-chengzheng@Yu-chengs-MacBook-Air.local>
2024-01-22 16:28:46 +0800

    update vite.config

--- vite.config.js
+++ vite.config.js
@@ -25,12 +25,13 @@ import { fileURLToPath, URL } from "url";
 export default defineConfig(({ command, mode }) => {
   // 根据当前工作目录中的 `mode` 加载 .env 文件
   // 设置第三个参数为 '' 来加载所有环境变量，而不管是否有 `VITE_` 前缀。
-  const env = loadEnv(mode, process.cwd(), '')
+  const env = loadEnv(mode, process.cwd())
+  const { VITE_BACKEND_HOST } = env;
   return {
     server: {
       proxy: {
         '/api': {
-          target: env.VITE_BACKEND_HOST,
+          target: VITE_BACKEND_HOST,
           changeOrigin: true,
           rewrite: (path) => path.replace(/^\/api/, ''),
         },
