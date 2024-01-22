4785ed4bccab79d976fce510b24fef9dce26956a
Yu-cheng Zheng <yu-chengzheng@Yu-chengs-MacBook-Air.local>
2024-01-22 16:19:27 +0800

    update vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,48 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from "url";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://cdn.nba.com',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+// export default defineConfig({
+//   server: {
+//     proxy: {
+//       '/api': {
+//         target: 'https://cdn.nba.com',
+//         changeOrigin: true,
+//         rewrite: (path) => path.replace(/^\/api/, ''),
+//       },
+//     },
+//   },
+//   plugins: [vue()],
+//   base: "/NBAScore",
+//   resolve: {
+//     alias: {
+//       "@": fileURLToPath(new URL("./src", import.meta.url)),
+//     },
+//   },
+// })
+
+export default defineConfig(({ command, mode }) => {
+  // 根据当前工作目录中的 `mode` 加载 .env 文件
+  // 设置第三个参数为 '' 来加载所有环境变量，而不管是否有 `VITE_` 前缀。
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_BACKEND_HOST,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        },
       },
     },
-  },
-  plugins: [vue()],
-  base: "/NBAScore",
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    plugins: [vue()],
+    base: "/NBAScore",
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+      },
     },
-  },
+  }
 })
+
