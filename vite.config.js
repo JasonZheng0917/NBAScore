a2c2f8269d228cbe56cb136cd06bfa4b1500985c
Yu-cheng Zheng <yu-chengzheng@Yu-chengs-MacBook-Air.local>
2024-01-23 13:43:56 +0800

    change base url

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { fileURLToPath, URL } from "url";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/NBAScore/",
+  base: "https://cdn.nba.com/static/json/liveData/scoreboard",
   server: {
     proxy: {
       '/api': {
