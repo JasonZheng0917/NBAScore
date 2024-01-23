351fd8a117cb233b043dd08586301ccafdbebbb5
Yu-cheng Zheng <yu-chengzheng@Yu-chengs-MacBook-Air.local>
2024-01-23 10:53:54 +0800

    update base url

--- vite.config.js
+++ vite.config.js
@@ -16,7 +16,7 @@ export default defineConfig({
     },
   },
   plugins: [vue()],
-  base: "/NBAScore",
+  base: "/NBAScore/",
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
