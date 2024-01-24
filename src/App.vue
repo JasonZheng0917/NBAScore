df916f050aa5ae55c99e7881f0895bc57b7cbfc4
Yu-cheng Zheng <yu-chengzheng@Yu-chengs-MacBook-Air.local>
2024-01-24 16:00:52 +0800

    change axios

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,7 @@ const asyncData = async () => {
     "https://cdn.nba.com/static/json/liveData/scoreboard/todaysScoreboard_00.json";
   try {
     const res = await axios({
-      url: "/NBAScore/api",
+      url: "/api",
       method: "get",
     });
 
